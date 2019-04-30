57c3c35a87c9f6be12bec056ea9681346e937aac
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-04-30 11:12:08 -0700

    add gitignore

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port:3306,
   user: 'root',
-  password: 'root',
-  database: 'loginnodejs'
+  password: '',
+  database: 'loginnode'
 });
 
 connection.connect(function (err) {
